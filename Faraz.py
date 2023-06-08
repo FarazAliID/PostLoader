30fd3039ea96f05cb0791c8387ee7a51bb69c311
Faraz Ali Jatoi <98204439+FARAZ-ID@users.noreply.github.com>
2023-06-08 23:35:45 +0500

    Update Faraz.py

--- Faraz.py
+++ Faraz.py
@@ -1,2 +1,19 @@
-from Loader_enc import tawasulxfaraz
-tawasulxfaraz()
+import os, sys, platform
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+	import Loader_enc
+
+else:
+
+	import Loader_enc
+
+elif bit == '32bit':
+
+	import Loader32
+
+else:
+
+	import Loader32
